20459a556a97904c41adbd1e115ff8750eeed422
Paul Zaich <pzaich@gmail.com>
2013-01-27 01:42:48 +0900

    modal somewhat working

--- app/controllers/paintings_controller.rb
+++ app/controllers/paintings_controller.rb
@@ -1,2 +1,42 @@
 class PaintingsController < ApplicationController
+  before_filter :find_gallery
+  before_filter :find_painting, :only => [:show, :edit, :update]
+  def show
+  end
+
+  def new
+    @painting = @gallery.paintings.new
+    respond_to do |f|
+      f.js {}
+    end
+  end
+
+  def create
+    @painting = @gallery.paintings.new(params[:params[:painting]])
+    if @painting.save
+      respond_to do |f|
+        f.js { }
+      end
+    else
+      respond_to do |f|
+        f.js {}
+      end
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+    def find_gallery
+      @gallery = Gallery.find(params[:gallery_id])
+    end
+
+    def find_painting
+      @painting = Painting.find(params[:id])
+    end
 end
