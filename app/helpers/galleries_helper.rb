df4aa17d72eb5e25642116fe98058ddc2854ad9c
Paul Zaich <pzaich@gmail.com>
2013-01-27 17:40:42 +0900

    get modal working properly

--- app/helpers/galleries_helper.rb
+++ app/helpers/galleries_helper.rb
@@ -1,5 +1,5 @@
 module GalleriesHelper
-  def render_modal(title, body, footer)
-    render 'shared/modal', :title => title, :body => body, :footer => footer
+  def render_modal(title, body, footer, object)
+    render 'shared/modal', :title => title, :body => body, :footer => footer, :object => object
   end
 end
